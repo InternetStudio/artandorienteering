e713cde25dcdbd74fbecfe0fa3c08b44de74ba12
alexharris <alexharris6@gmail.com>
2014-03-11 14:52:43 -0700

    initial commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer role="contentinfo">
+    <p>Online catalogue for FEATURED FEATURES, a project by Hannah Jickling hosted at The Yukon Arts Centre Gallery, August – September, 2013. </p>
+	</footer>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
