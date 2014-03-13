7117efdc15c3e68d1f6cca31b583d62b4240823d
alexharris <alexharris6@gmail.com>
2014-03-13 11:20:26 -0700

    Add background image, tweak text styles, remove page titles

--- content-page.php
+++ content-page.php
@@ -9,13 +9,6 @@
 ?>
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-			<?php if ( ! is_page_template( 'page-templates/front-page.php' ) ) : ?>
-			<?php the_post_thumbnail(); ?>
-			<?php endif; ?>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-		</header>
-
 		<div class="entry-content">
 			<?php the_content(); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
