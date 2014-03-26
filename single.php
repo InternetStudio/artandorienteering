32da339fdd93a6d9c45fadbb72cd21b3c22df2c3
alexharris <alexharris6@gmail.com>
2014-03-26 14:43:41 -0700

    fix paragraph line height, reverse prev/next scroller on posts

--- single.php
+++ single.php
@@ -18,8 +18,9 @@ get_header(); ?>
 
 				<nav class="nav-single">
 					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
-					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
+					<span class="nav-previous"><?php next_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Next post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
+					<span class="nav-next"><?php previous_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Previous post link', 'twentytwelve' ) . '</span>' ); ?></span>
+		
 				</nav><!-- .nav-single -->
 
 
