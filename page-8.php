2a22baf3d15067f2dbfe17b0c5db42bf87233775
alexharris <alexharris6@gmail.com>
2014-06-23 15:56:32 -0700

    new fonts, include page text at top of category pages

---
+++ page-8.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+
+	
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
+			<?php endwhile; // end of the loop. ?>
+
+			<?php ("cat=4"); ?>
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
+
+					/* Include the post format-specific template for the content. If you want to
+					 * this in a child theme then include a file called called content-___.php
+					 * (where ___ is the post format) and that will be used instead.
+					 */
+					get_template_part( 'content', get_post_format() );
+
+				endwhile;
+				?>
+
+			<?php else : ?>
+				<?php get_template_part( 'content', 'none' ); ?>
+			<?php endif; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
